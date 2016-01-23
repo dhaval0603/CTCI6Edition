de6951b67010e458afd2b518f5f0a6a1b6f46dd0
dhaval0603 <dhaval0603@gmail.com>
2016-01-23 16:38:35 -0500

    Java : Chapter 1 Solution 3 added : URLify

---
+++ Java/1-ArraysStrings/URLify.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class URLify {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String : ");
+		String inputString = sc.nextLine();
+		System.out.println("Enter true length : ");
+		int trueLength = sc.nextInt();
+
+		System.out.println("URL version of the string : " + urlify(inputString, trueLength));
+	}
+
+	public static String urlify(String inputString, int trueLength) {
+		int countSpace = 0;
+		char input[] = inputString.toCharArray();
+
+		// Count number of spaces to compute the resulting string length
+		for (int i = 0; i < trueLength; i++) {
+			if (input[i] == ' ')
+				countSpace++;
+		}
+
+		int index = trueLength + 2*countSpace -1;
+		for (int i = trueLength -1; i >= 0; i--) {
+			if (input[i] == ' ') {
+				// If 'space', replace by '%20'
+				input[index - 2] = '%';
+				input[index - 1] = '2';
+				input[index] = '0';
+				index = index - 3;
+			} else {
+				// Else copy the char as it is
+				input[index] = input[i];
+				index--;
+			}
+		}
+
+		String resultString = new String(input);
+		return resultString;
+	}
+}
